033ca1dd34aae1853aa8ed85c68721203b306fde
akumar <akumar@trapptechnology.com>
2024-05-01 19:19:36 -0700

    Header mangling removed

--- ReadJsonConfiguration.h
+++ ReadJsonConfiguration.h
@@ -1,12 +1,4 @@
 #include "ndpi_includes.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int IsValidFlowForLogging(struct ndpi_flow_info* flow);
 void FreeSettingsConfigurationData();
 
-#ifdef __cplusplus
-}
-#endif
