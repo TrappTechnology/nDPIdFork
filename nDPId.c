36eccbda14fdf28ad241376ba128d62e202d12e4
akumar <akumar@trapptechnology.com>
2024-05-01 18:49:42 -0700

    Add Formatter and Json-c library folder

--- nDPId.c
+++ nDPId.c
@@ -33,6 +33,7 @@
 #include "nDPIsrvd.h"
 #include "nio.h"
 #include "utils.h"
+#include "ReadJsonConfiguration.h"
 
 #ifndef UNIX_PATH_MAX
 #define UNIX_PATH_MAX 108
@@ -2397,6 +2398,12 @@ static void serialize_and_send(struct nDPId_reader_thread * const reader_thread)
     uint32_t json_msg_len;
 
     json_msg = ndpi_serializer_get_buffer(&reader_thread->workflow->ndpi_serializer, &json_msg_len);
+
+    char * converted_json_str = NULL;
+    int createAlert = 0;
+    ConvertnDPIDataFormat(json_msg, &converted_json_str, &createAlert);
+    free(converted_json_str);
+
     if (json_msg == NULL || json_msg_len == 0)
     {
         logger(1,
