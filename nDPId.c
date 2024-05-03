cecbb07cd66dcd4684c23fae91253809c069f9a0
akumar <akumar@trapptechnology.com>
2024-05-02 19:11:45 -0700

    comment out code in send_to_collector

--- nDPId.c
+++ nDPId.c
@@ -2328,7 +2328,7 @@ static void send_to_collector(struct nDPId_reader_thread * const reader_thread,
         }
     }
 
-    errno = 0;
+  /*  errno = 0;
     ssize_t written;
     if (reader_thread->collector_sock_last_errno == 0 &&
         (written = write(reader_thread->collector_sockfd, newline_json_msg, s_ret)) != s_ret)
@@ -2389,7 +2389,7 @@ static void send_to_collector(struct nDPId_reader_thread * const reader_thread,
             }
             set_collector_nonblock(reader_thread);
         }
-    }
+    }*/
 }
 
 static void serialize_and_send(struct nDPId_reader_thread * const reader_thread)
