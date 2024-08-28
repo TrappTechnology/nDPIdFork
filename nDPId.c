3e9805e31a8643959f9c2bdd34a275bdde23cffd
akumar <akumar@trapptechnology.com>
2024-08-27 17:34:28 -0700

    2

--- nDPId.c
+++ nDPId.c
@@ -2358,13 +2358,16 @@ static void jsonize_flow(struct nDPId_workflow * const workflow, struct nDPId_fl
 
     printf("\nAshwani: SERVER 6 2");
 
+    nDPId_detection_data * detection_data = flow->info.detection_data;
 
+    printf("\nAshwani: SERVER 6 3") ;
+    if (detection_data != NULL)
+    {
+        printf("\nAshwani: SERVER 6 4");
+        ndpi_serialize_string_string(&workflow->ndpi_serializer, "hostname", &flow->info.detection_data->flow.host_server_name);
+        printf("\nAshwani: SERVER 6 5");
+    }
 
-    ndpi_serialize_string_string(&workflow->ndpi_serializer,
-                                 "hostname",
-                                 flow->info.detection_data->flow.host_server_name);
-
-    printf("\nAshwani: HOSTNAME %s");
     printf("\nAshwani HOST SERVER NAME End");
     // Ashwani End here
 
