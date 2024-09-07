d0ac1ac00a2510ce572a6657f07cc3660391eb3b
akumar <akumar@trapptechnology.com>
2024-09-07 13:32:09 -0700

    Remove code to eliminate duplicate entries

--- nDPId.c
+++ nDPId.c
@@ -675,48 +675,49 @@ void ensure_capacity(FlowMap * map)
 // Add or update an entry in the FlowMap
 void add_or_update_flow_entry(FlowMap * map, int flow_id, int flow_event_id, int packet_id, const char * json_str, const char * json_str_alert)
 {
+    
     // Check if the flow_id already exists
-    logger(0, "ASHWANI: add_or_update_flow_entry 1");
+  
     if (map == NULL) 
     {
         return;
     }
     for (size_t i = 0; i < map->size; ++i)
     {
-        logger(0, "ASHWANI: add_or_update_flow_entry 2");
-        if (map->entries[i].flow_id == flow_id && (map->entries[i].flow_event_id <= flow_event_id) &&  (map->entries[i].packet_id <= packet_id))
-        {
-            logger(0, "ASHWANI: add_or_update_flow_entry 3");
-            // Update existing entry
-            if (json_str != NULL)
-            {
-                free(map->entries[i].json_str);
-                map->entries[i].json_str = strdup(json_str);
-            }
+        //logger(0, "ASHWANI: add_or_update_flow_entry 2");
+        //if (map->entries[i].flow_id == flow_id && (map->entries[i].flow_event_id <= flow_event_id) &&  (map->entries[i].packet_id <= packet_id))
+        //{
+        //    logger(0, "ASHWANI: add_or_update_flow_entry 3");
+        //    // Update existing entry
+        //    if (json_str != NULL)
+        //    {
+        //        free(map->entries[i].json_str);
+        //        map->entries[i].json_str = strdup(json_str);
+        //    }
 
-            // Update existing entry
-            if (json_str_alert != NULL)
-            {
-                free(map->entries[i].json_str_alert);
-                map->entries[i].json_str_alert = strdup(json_str_alert);
-            }
+        //    // Update existing entry
+        //    if (json_str_alert != NULL)
+        //    {
+        //        free(map->entries[i].json_str_alert);
+        //        map->entries[i].json_str_alert = strdup(json_str_alert);
+        //    }
 
-            logger(0, "ASHWANI: add_or_update_flow_entry 4");
-            return;
-        }
+        //    logger(0, "ASHWANI: add_or_update_flow_entry 4");
+        //    return;
+        //}
     }
 
     // Add new entry
-    logger(0, "ASHWANI: add_or_update_flow_entry 5");
+   
     ensure_capacity(map);
-    logger(0, "ASHWANI: add_or_update_flow_entry 6");
+   
     map->entries[map->size].flow_id = flow_id;
     map->entries[map->size].flow_event_id = flow_event_id;
     map->entries[map->size].packet_id = packet_id;
     map->entries[map->size].json_str = strdup(json_str);
     map->entries[map->size].json_str_alert = NULL;
 
-    logger(0, "ASHWANI: add_or_update_flow_entry 7");
+  
     if (json_str_alert != NULL)
     {
         map->entries[map->size].json_str_alert = strdup(json_str_alert);
