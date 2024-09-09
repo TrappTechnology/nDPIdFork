3ae5789332867e3a12774a438145be657c7fd151
akumar <akumar@trapptechnology.com>
2024-09-08 21:53:46 -0700

    Memory leak

--- nDPId.c
+++ nDPId.c
@@ -677,13 +677,17 @@ void ensure_capacity(FlowMap * map)
 static void update_xfer_if_greater(char * json_str1, const char * json_str2)
 {
     // Parse the JSON strings
+    logger(0, "ASHWANI: update_xfer_if_greater 1");
     struct json_object * json1 = json_tokener_parse(json_str1);
     struct json_object * json2 = json_tokener_parse(json_str2);
 
+     logger(0, "ASHWANI: update_xfer_if_greater 2");
     // Get the "xfer" fields from both JSON objects
     struct json_object *xfer1, *xfer2;
     json_object_object_get_ex(json1, "xfer", &xfer1);
     json_object_object_get_ex(json2, "xfer", &xfer2);
+
+     logger(0, "ASHWANI: update_xfer_if_greater 3");
 
     // Extract the "source" and "destination" fields from "xfer"
     struct json_object *source1, *destination1, *source2, *destination2;
@@ -692,41 +696,57 @@ static void update_xfer_if_greater(char * json_str1, const char * json_str2)
     json_object_object_get_ex(xfer2, "source", &source2);
     json_object_object_get_ex(xfer2, "destination", &destination2);
 
+     logger(0, "ASHWANI: update_xfer_if_greater 4");
+
     // Extract the "packets" and "bytes" from both "source" and "destination"
     int src1_packets = json_object_get_int(json_object_object_get(source1, "packets"));
     int src1_bytes = json_object_get_int(json_object_object_get(source1, "bytes"));
     int dst1_packets = json_object_get_int(json_object_object_get(destination1, "packets"));
     int dst1_bytes = json_object_get_int(json_object_object_get(destination1, "bytes"));
 
+     logger(0, "ASHWANI: update_xfer_if_greater 5");
+
     int src2_packets = json_object_get_int(json_object_object_get(source2, "packets"));
     int src2_bytes = json_object_get_int(json_object_object_get(source2, "bytes"));
     int dst2_packets = json_object_get_int(json_object_object_get(destination2, "packets"));
     int dst2_bytes = json_object_get_int(json_object_object_get(destination2, "bytes"));
 
+     logger(0, "ASHWANI: update_xfer_if_greater 6");
+
     // Update values in json1 if values in json2 are greater
     if (src2_packets > src1_packets)
     {
+         logger(0, "ASHWANI: update_xfer_if_greater 7");
         json_object_object_add(source1, "packets", json_object_new_int(src2_packets));
     }
 
     if (src2_bytes > src1_bytes)
     {
+        logger(0, "ASHWANI: update_xfer_if_greater 8");
         json_object_object_add(source1, "bytes", json_object_new_int(src2_bytes));
     }
 
     if (dst2_packets > dst1_packets)
     {
+        logger(0, "ASHWANI: update_xfer_if_greater 9");
         json_object_object_add(destination1, "packets", json_object_new_int(dst2_packets));
     }
 
     if (dst2_bytes > dst1_bytes)
     {
+        logger(0, "ASHWANI: update_xfer_if_greater 10");
         json_object_object_add(destination1, "bytes", json_object_new_int(dst2_bytes));
     }
 
     // Clean up
+
+    logger(0, "ASHWANI: update_xfer_if_greater 11");
     json_object_put(json1);
+
+    logger(0, "ASHWANI: update_xfer_if_greater 12");
     json_object_put(json2);
+
+    logger(0, "ASHWANI: update_xfer_if_greater 13");
 }
 
 
