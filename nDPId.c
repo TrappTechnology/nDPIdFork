c5f84c8a0849e966f407bd2bf730c22198e6a68e
akumar <akumar@trapptechnology.com>
2024-10-20 18:28:16 -0700

    // printf("\n%s\n", json_msg);

--- nDPId.c
+++ nDPId.c
@@ -2738,7 +2738,7 @@ static void send_to_collector( struct nDPId_reader_thread * const reader_thread,
                      (int)json_msg_len,
                      json_msg);
 
-    printf("\n%s\n", json_msg);
+    // printf("\n%s\n", json_msg);
     if (s_ret < 0 || s_ret >= (int)sizeof(newline_json_msg))
     {
         logger(1,
