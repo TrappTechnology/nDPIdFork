302592ae7452885edc378fb9382a7753ee7e44a7
akumar <akumar@trapptechnology.com>
2024-11-11 19:04:18 -0800

    more logging

--- nDPId.c
+++ nDPId.c
@@ -2761,7 +2761,7 @@ static write_to_file(const char * json_str, size_t json_msg_len)
     ConvertnDPIDataFormat(json_str, &converted_json_str, &flowRisksCount, &flow_id, &flow_event_id, &packet_id, 0);
     if (flow_id != 834264320534 && converted_json_str != NULL)
     {
-
+        logger(0, "ASHWANI Stage 1 %s", converted_json_str);
         int length = strlen(converted_json_str);
         if (duplicate_data(converted_json_str, length))
         {
