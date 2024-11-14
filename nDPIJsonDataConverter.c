af080483b58493bd48dc440569f2efaeefa51d2b
akumar <akumar@trapptechnology.com>
2024-11-13 17:08:00 -0800

    minor

--- nDPIJsonDataConverter.c
+++ nDPIJsonDataConverter.c
@@ -1483,23 +1483,23 @@ void UpdateXferIfGreater(char * existing_json_str, const char * new_json_str, ch
     if (strcmp(new_event_end_string, existing_event_end_string) > 0) 
     {
         json_object_object_del(existing_event_obj, "end");
-        json_object_object_add(existing_event_obj, "end", json_object_new_string(new_event_end_string));
-
-         // update event.duration field
-        struct json_object *existing_event_duration, *new_event_duration;
-        json_object_object_get_ex(existing_event_obj, "duration", &existing_event_duration);
-        json_object_object_get_ex(new_event_obj, "duration", &new_event_duration);
-        unsigned long existing_event_duration_value = json_object_get_int(existing_event_duration);
-        unsigned long new_event_duration_value = json_object_get_int(new_event_duration);
-      
-        logger(0, "Ashwani 4 %lu", new_event_duration_value);
-        logger(0, "Ashwani 5 %lu", existing_event_duration_value);
-        if (new_event_duration_value > existing_event_duration_value)
-        {
-            json_object_object_del(existing_event_obj, "duration");        
-            json_object_object_add(existing_event_obj, "duration", json_object_new_int64(new_event_duration_value));              
-        }          
+        json_object_object_add(existing_event_obj, "end", json_object_new_string(new_event_end_string));        
     }
+
+    // update event.duration field
+    struct json_object *existing_event_duration, *new_event_duration;
+    json_object_object_get_ex(existing_event_obj, "duration", &existing_event_duration);
+    json_object_object_get_ex(new_event_obj, "duration", &new_event_duration);
+    unsigned long existing_event_duration_value = json_object_get_int(existing_event_duration);
+    unsigned long new_event_duration_value = json_object_get_int(new_event_duration);
+
+    logger(0, "Ashwani 4 %lu", new_event_duration_value);
+    logger(0, "Ashwani 5 %lu", existing_event_duration_value);
+    if (new_event_duration_value > existing_event_duration_value)
+    {
+        json_object_object_del(existing_event_obj, "duration");
+        json_object_object_add(existing_event_obj, "duration", json_object_new_int64(new_event_duration_value));
+    }  
 
     free(existing_event_end_string);
     free(new_event_end_string);
