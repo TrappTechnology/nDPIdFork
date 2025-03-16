b1cc3c46c55e0c5d0057412cd58e80124ddffe88
akumar <akumar@trapptechnology.com>
2025-03-16 06:29:21 -0700

    Bug fixing

--- nDPId.c
+++ nDPId.c
@@ -2716,7 +2716,7 @@ static write_to_file(const char * json_str, size_t json_msg_len)
     // Ashwani: uncomment this to put data to console
     // printf("\n%s\n", json_str);
 
-    ConvertnDPIDataFormat(json_str, &converted_json_str, &flowRisksCount, &flow_id, &flow_event_id, &packet_id, 0);
+    ConvertnDPIDataFormat(json_str, &converted_json_str, &flowRisksCount, &flow_id, &flow_event_id, &packet_id, current_pcap_file);
     if (flow_id != 834264320534 && converted_json_str != NULL)
     {
         int length = strlen(converted_json_str);
