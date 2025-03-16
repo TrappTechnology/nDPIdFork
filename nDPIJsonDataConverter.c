92735c39b6568ce7155fcf11be5284760973234c
akumar <akumar@trapptechnology.com>
2025-03-16 06:03:06 -0700

    minor issue

--- nDPIJsonDataConverter.c
+++ nDPIJsonDataConverter.c
@@ -1313,7 +1313,7 @@ static void add_Root_Data(json_object ** root_object,
     json_object * event_object = json_object_new_object();
     json_object_object_add(event_object, "start", json_object_new_string(creationTimeStr));
     json_object_object_add(event_object, "end", json_object_new_string(modificationTimeStr));
-    json_object_object_add(event_object, "duration", json_object_get_double(duration_nanoseconds));
+    json_object_object_add(event_object, "duration", json_object_new_double(duration_nanoseconds));
 
     if (flowRiskCount > 0)
     {
