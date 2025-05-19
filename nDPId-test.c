0cadcb96e3efbc071c76cd85409caa744931bc99
akumar <akumar@trapptechnology.com>
2025-05-19 16:48:16 -0700

    enp4s0

--- nDPId-test.c
+++ nDPId-test.c
@@ -2047,7 +2047,8 @@ int main(int argc, char ** argv)
     currentFileIndex = 0;
     for (currentFileIndex = 0; currentFileIndex < number_of_valid_files_found; currentFileIndex++)
     {
-        set_cmdarg(&nDPId_options.pcap_file_or_interface, "enp3s0");
+        //set_cmdarg(&nDPId_options.pcap_file_or_interface, "enp3s0");
+        set_cmdarg(&nDPId_options.pcap_file_or_interface, "enp4s0");
         logger(0, "%d. processing of %s file started------------------------------------------------", currentFileIndex+1,pcap_files[currentFileIndex]);
 
         //char pcap_error_buffer[PCAP_ERRBUF_SIZE];
