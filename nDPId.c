317822307e67f083cc645e4ee9e68f8c33be894f
akumar <akumar@trapptechnology.com>
2025-05-20 15:19:46 -0700

    pcap_open_live

--- nDPId.c
+++ nDPId.c
@@ -1516,15 +1516,15 @@ static struct nDPId_workflow * init_workflow(char const * const file_or_device)
     if (access(file_or_device, R_OK) != 0 && errno == ENOENT)
     {
         // AShwani
-        logger(0, "calling pcap_open_live");
-        //workflow->pcap_handle = pcap_open_live(file_or_device, 65535, 1, 1, pcap_error_buffer);
+        logger(0, "Ashwani: calling pcap_open_live");
+        workflow->pcap_handle = pcap_open_live(file_or_device, 65535, 1, 1, pcap_error_buffer);
 
-        pcap_t * handle = pcap_create(file_or_device, pcap_error_buffer);
-        pcap_set_snaplen(handle, 65535);
-        pcap_set_promisc(handle, 1);
-        pcap_set_timeout(handle, 1);
-        pcap_set_buffer_size(handle, 8 * 1024 * 1024); // 8 MB buffer
-        pcap_activate(handle);
+        //pcap_t * handle = pcap_create(file_or_device, pcap_error_buffer);
+        //pcap_set_snaplen(handle, 65535);
+        //pcap_set_promisc(handle, 1);
+        //pcap_set_timeout(handle, 1);
+        //pcap_set_buffer_size(handle, 8 * 1024 * 1024); // 8 MB buffer
+        //pcap_activate(handle);
 
     }
     else
